d47905ae9de87c9dcf200cac473aae6134365a4d
totekits <tote.kits@gmail.com>
2023-11-02 15:54:18 +0700

    Refactor the code

--- lib/main.rb
+++ lib/main.rb
@@ -1,117 +1,149 @@
 class Hangman
-  def play
+
+  def initialize
+    @winner = false
+    @guesses = 7
+    @keyword = []
+    @clue = []
+    @right_letters = []
+    @wrong_letters = []
+  end
+
+  def game
     greet
-    game
-    finish
   end
 
   private
 
-  def greet
-    loop do 
-      puts "Welcome to Hangman!\n" +
-        "You have 7 guesses to guess the keyword\n" +
-        "Type in the command and press Enter\n" +
-        "'new'\tto start a new game\n" +
-        "'load'\tto load a saved game\n" +
-        "'exit'\tto exit the game"
-        
-      choice = gets.chomp.downcase
-
-      if choice == "new"
-        break
-      elsif choice == "load"
-        load_game
-      elsif choice == "exit"
-        exit
-      else
-        puts "Invalid command. Try again."
-      end
-    end
+  def display_main_menu
+    puts "Enter a command:\n" +
+      "'new'\tto start a new game\n" +
+      "'load'\tto load a saved game\n" +
+      "'exit'\tto exit the game"
   end
 
   def pick_random_word
-    words = File.readlines("google-10000-english-no-swears.txt").map(&:chomp)
-    words.select { |word| (5..12).cover?(word.size) }.sample.chars
+    dictionary = File.readlines("google-10000-english-no-swears.txt").map(&:chomp)
+    dictionary.select { |word| (5..12).cover?(word.size) }.sample.chars
   end
 
-  def display(guesses, clues, wrongs)
-    puts "Guesses left: #{guesses}\n" +
-      "Wrong characters: #{wrongs.join(" ")}\n" +
-      "#{clues.join(" ")}\n" +
-      "Type in a character and press Enter to guess\n" +
-      "Type 'save' and press Enter to save the game and quit\n" +
-      "Type 'exit' and press Enter to quit the game"
+  def new_game
+    puts "You must guess the keyword.\n" +
+      "You lose if you pick the wrong letters 7 times.\n" +
+      "Good luck!"
+
+    @keyword = pick_random_word
+    @clue = Array.new(@keyword.size, "_")
+
+    play
   end
 
-  def finish(player_wins, keyword)
-    if player_wins == true
-      puts "You win!"
+  def process_main_menu(choice)
+    case choice
+    when "new"
+      new_game
+    when "load"
+      load_game
+    when "exit"
+      exit
     else
-      puts "You lose!\n" +
-        "The keyword is #{keyword.join}"
-    end
-
-    loop do
-      puts "Type 'new' and press Enter to start a new game\n" +
-        "Type 'load' and press Enter to load a saved game\n"
-        "Type 'exit' and press Enter to quit the game"
-
-      choice = gets.chomp.downcase
-      if choice == "new"
-        game
-      elsif choice == "load"
-        load_game
-      elsif choice == "exit"
-        exit
-      else
-        puts "Invalid command. Try again."
-      end
+      puts "Invalid command. Try again."
     end
   end
 
-  def game
-    guesses = 7
-    player_wins = false
-    keyword = pick_random_word
-    clues = []
-    (keyword.size).times { clues << "_" }
-    rights = []
-    wrongs = []
+  def greet
+    puts "Welcome to Hangman!"
+    display_main_menu
 
     loop do
-      display(guesses, clues, wrongs)
       choice = gets.chomp.downcase
-      if choice == "save" 
-        save_game
-      elsif choice == "exit"
-        exit
-      elsif wrongs.include?(choice) || rights.include?(choice)
-        puts "You've already picked '#{choice}! Try again."
-      elsif !choice.match?(/^[a-z]$/)
-        puts "Invalid guess. Try again."
-      else
-        if keyword.include?(choice)
-          keyword.each_with_index do |char, index|
-            if char == choice
-              clues[index] = choice 
-            end
+      process_main_menu(choice)
+    end
+  end
+
+  def display_game_state
+    puts "Guesses left: #{@guesses}\n" +
+      "Wrong letters: #{@wrong_letters.join(" ")}\n" +
+      "#{@clue.join(" ")}\n"
+  end
+
+  def display_game_menu
+    puts "Enter a letter [a-z] to guess, or a command:\n" + 
+      "'new'\tto start a new game\n" +
+      "'save\tto save the game and exit\n" +
+      "'load'\tto load a saved game\n" +
+      "'exit'\tto exit the game"
+  end
+
+  def process_game_menu(choice)
+    case choice
+    when "new"
+      new_game
+    when "save"
+      save_game
+    when "load"
+      load_game
+    when "exit"
+      exit
+    else
+      return choice
+    end
+  end
+
+  def process_guess(choice)
+    if !choice.match?(/^[a-z]$/)
+      puts "Invalid command. Try again."
+    elsif @wrong_letters.include?(choice) || @right_letters.include?(choice)
+      puts "You've already picked '#{choice}! Try again."
+    else 
+      if @keyword.include?(choice)
+        @keyword.each_with_index do |char, index|
+          if char == choice
+            @clue[index] = choice 
           end
-          rights << choice
-        else 
-          wrongs << choice
-          guesses -= 1
         end
+        @right_letters << choice
+      else 
+        @wrong_letters << choice
+        @guesses -= 1
       end
-      
-      if clues == keyword
-        player_wins = true
-        finish(player_wins, keyword)
-      elsif guesses == 0
-        finish(player_wins, keyword)
-      end
+    end  
+  end
+
+  def thank
+    puts "Thanks for trying my game!"
+    display_main_menu
+
+    loop do
+      choice = gets.chomp.downcase
+      process_main_menu(choice)
     end
   end
+
+  def check_win
+    if @clue.join == @keyword.join
+      puts "You win!\n" +
+        "The keyword is '#{@keyword.join}'"
+      @winner = true
+    elsif @guesses == 0
+      puts "You lose!\n" +
+        "The keyword is '#{@keyword.join}'"
+      @winner = true
+    end
+  end
+
+  def play
+    loop do
+      display_game_state
+      display_game_menu
+      choice = gets.chomp.downcase
+      process_game_menu(choice)
+      process_guess(choice)
+      check_win
+      break if @winner
+    end
+    thank
+  end  
 end
 
-Hangman.new.play
+Hangman.new.game
